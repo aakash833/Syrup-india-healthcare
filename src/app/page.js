658b11e9c3c1d530d5dea7b142b143bc8db7900d
aakash-7span <aakash@7span.com>
2024-08-02 17:23:52 +0530

    name changed

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By Aakash
+            By Aakash & Bhargav
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
