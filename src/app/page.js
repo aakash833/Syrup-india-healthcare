46e99a2e63280e32f1301a46e517cc073f1f626b
Aakash Vamja <141322422+aakash-7span@users.noreply.github.com>
2024-08-28 19:08:01 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By Aakash & Bhargav
+            By Aakash
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
